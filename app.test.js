d51a76c16a656ea9d8b11b33498c1595bdedf1ca
YashZ2002 <zaveri.y@northeastern.edu>
2024-10-19 18:34:50 -0400

    test

--- app.test.js
+++ app.test.js
@@ -6,27 +6,27 @@ import sequelize from "./src/config/db.js";
 
 describe("Create an account and using GET, validate account exists", () => {
     test("should respond with a 200 status code", async () => {
-        await request(app).post("/user/create-user").send({
+        await request(app).post("/user/v1/create-user").send({
             email: "jaygala25@gmail.com",
             password: "123456",
             firstName: "Jay",
             lastName: "Gala"
         });
 
-        const response = await request(app).get("/user/get-user").auth("jaygala25@gmail.com", "123456")
+        const response = await request(app).get("/user/v1/get-user").auth("jaygala25@gmail.com", "123456")
         expect(response.status).toBe(200);
     })
 });
 
 describe("Update the account and using GET, validate the account was updated", () => {
     test("should respond with a 200 status code", async () => {
-        await request(app).put("/user/update-user").auth("jaygala25@gmail.com", "123456").send({
+        await request(app).put("/user/v1/update-user").auth("jaygala25@gmail.com", "123456").send({
             firstName: "Akshay",
             lastName: "Dedhia",
             password: "123456"
         });
 
-        const response = await request(app).get("/user/get-user").auth("jaygala25@gmail.com", "123456")
+        const response = await request(app).get("/user/v1/get-user").auth("jaygala25@gmail.com", "123456")
         expect(response.status).toBe(200);
         expect(response.body.first_name).toBe("Akshay");
         expect(response.body.last_name).toBe("Dedhia");
